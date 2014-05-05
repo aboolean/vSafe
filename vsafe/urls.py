963f91f27456677d9300e3d6a5ee525a7d95498e
Elliott Marquez <marquez@mit.edu>
2014-05-04 21:24:26 -0400

    attempt to fix static folder

--- vsafe/urls.py
+++ vsafe/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.base import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +17,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'website.views.index'),
 )
-  
+
+urlpatterns += staticfiles_urlpatterns()
