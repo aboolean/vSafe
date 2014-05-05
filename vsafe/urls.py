a0c458e3237b0087dd0b945e6cf8a3722e62b946
Elliott Marquez <marquez@mit.edu>
2014-05-04 21:15:30 -0400

    put the wrong path to the index

--- vsafe/urls.py
+++ vsafe/urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'vsafe.views.index'),
+    url(r'^$', 'website.views.index'),
 )
   
