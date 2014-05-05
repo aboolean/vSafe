30cdcd53897532887e6faccc324e24a30a1f9cdc
Elliott Marquez <marquez@mit.edu>
2014-05-04 21:04:25 -0400

    created basic view and url

--- vsafe/urls.py
+++ vsafe/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+    url(r'^$', 'vsafe.views.index'),
 )
+  
--- website/views.py
+++ website/views.py
@@ -1,1 +1,8 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
 # Create your views here.
+def index (request):
+	t = get_template('index.html')
+	html = t.render()
+	return HttpResponse(html)
