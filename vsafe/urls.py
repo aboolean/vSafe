3dd1fe85617e56e432f5b5c31280c1a87c1aaf3d
Andre Aboulian <aandre@mit.edu>
2014-05-05 05:54:08 -0400

    format transitioned

--- vsafe/urls.py
+++ vsafe/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from website import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,14 +17,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'website.views.index'),
-    url(r'^learn/', 'website.views.learn'),
-    url(r'^browse/$', 'website.views.browse'),
-    url(r'^browse/ingredient/[0-9]*$', 'website.views.ingredient'),
-    url(r'^browse/dish/[0-9]*$', 'website.views.dish'),
-
-    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': path.join(path.dirname(__file__), 'static')}),
-    # url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/static/'}),
+    url(r'^$', views.index),
+    url(r'^learn/', views.learn),
+    url(r'^browse/$', views.browse),
+    url(r'^browse/ingredient/[0-9]*$', views.ingredient),
+    url(r'^browse/dish/[0-9]*$', views.dish),
 )
-
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
