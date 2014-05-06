2c1ed96669efff801cec7d9649b1324b12f24a4d
Elliott Marquez <marquez@mit.edu>
2014-05-05 23:45:10 -0400

    tried to get the substitutes in the views

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from django.template import Context
 from models import *
+import itertools
 # Create your views here.
 def index (request):
 	t = get_template('index.html')
@@ -28,7 +29,10 @@ def ingredient (request, id_num):
 def dish (request, id_num):
 	t = get_template('dish.html')
 	dish_object = Dish.objects.get(id=id_num)
-	c = Context({'dish':dish_object})
+	substitutes = [ingred.substitutes.all() for ingred in dish_object.ingredients.all()]
+	substitutes = list(itertools.chain.from_iterable(substitutes)).sort()
+	substitutes = list(substitutes for substitutes,_ in itertools.groupby(substitutes))
+	c = Context({'dish':dish_object, 'subs':substitutes})
 	html = t.render(c)
 	return HttpResponse(html)
 
