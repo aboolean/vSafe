65d623b0d2d546e578b5bdd4e27c69dec2ca87d5
Andre Aboulian <aandre@mit.edu>
2014-05-06 04:48:18 -0400

    /subset/ url added

--- vsafe/urls.py
+++ vsafe/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns('',
     url(r'^browse/?$', views.browse),
     url(r'^browse/ingredient/(?P<id_num>[0-9]+)/?$', views.ingredient),
     url(r'^browse/dish/(?P<id_num>[0-9]+)/?$', views.dish),
+    url(r'^browse/subset/', views.subset),
 )
